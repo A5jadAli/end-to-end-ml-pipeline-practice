9d14fa8a5be1965464ae4227c82c3d3be587809a
Asjad <aliasjid009@gmail.com>
2024-12-26 16:38:47 +0500

    pipeline configured using DVC with params

--- src/model_building.py
+++ src/model_building.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import logging
 from typing import Tuple
+import yaml
 
 # Ensure the "logs" directory exists
 log_dir = "logs"
@@ -27,6 +28,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_engineered_data(data_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Load the engineered train and test datasets."""
@@ -97,7 +115,8 @@ def save_model(model: RandomForestClassifier, model_path: str) -> None:
 
 def main():
     try:
-        params = {'n_estimators':100, 'max_depth': 10, 'min_samples_split':2, 'min_samples_leaf':1, 'random_state':42}
+        params = load_params('params.yaml')['model_building']
+        # params = {'n_estimators':100, 'max_depth': 10, 'min_samples_split':2, 'min_samples_leaf':1, 'random_state':42}
         # Load the engineered train and test data
         train_data, test_data = load_engineered_data(data_path='./data')
         
