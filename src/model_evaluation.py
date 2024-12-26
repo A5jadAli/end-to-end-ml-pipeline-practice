fd998264d083822fe0345c132c8a4b9cb298ee09
Asjad <aliasjid009@gmail.com>
2024-12-26 19:26:27 +0500

    experiment tracking using dvclive done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,6 +6,8 @@ import json
 import pickle
 import logging
 from typing import Dict, Tuple
+import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = "logs"
@@ -28,6 +30,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 def load_engineered_data(data_path: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Load the engineered train and test datasets."""
@@ -112,6 +131,7 @@ def save_metrics(metrics: Dict, report_path: str) -> None:
 
 def main():
     try:
+        params = load_params(params_path='params.yaml')
         # Load the engineered data
         train_data, test_data = load_engineered_data(data_path='./data')
         
@@ -123,6 +143,14 @@ def main():
         
         # Evaluate the model
         metrics = evaluate_model(model, X_test, y_test)
+        
+        # experiment tracking with DVC
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+            
+            live.log_params(params)
         
         # Save the metrics
         save_metrics(metrics, report_path='./reports')
